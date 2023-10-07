ca3516ad973e52d71975601a5c14b8966cdbb908
Shaun Inman <shaun@panic.com>
2023-10-07 11:59:18 -0400

    added toolchain file for cmake

---
+++ toolchain.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_SYSROOT /work/JELOS/build.JELOS-RK3566.aarch64/toolchain/aarch64-libreelec-linux-gnueabi/sysroot)
+set(CMAKE_STAGING_PREFIX /tmp)
+
+set(tools /work/JELOS/build.JELOS-RK3566.aarch64/toolchain)
+set(CMAKE_C_COMPILER ${tools}/bin/aarch64-libreelec-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/aarch64-libreelec-linux-gnueabi-g++)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_FIND_NO_INSTALL_PREFIX)
